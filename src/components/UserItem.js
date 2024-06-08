4a4290765d4a12ca9924920a007885a85c13d97b
rizkih1109 <rizkihidayatulloh1305@gmail.com>
2024-06-08 11:08:04 +0700

    User Item done

---
+++ src/components/UserItem.js
@@ -0,0 +1,17 @@
+export default function UserItem({ user }) {
+    return (
+        <div className="userCard cardPosition">
+            <div>
+                <img src='../pictures/bachira.jpeg' alt='bachira'></img>
+            </div>
+            <div className="list">
+                <p>{user.name}</p>
+                <p>{user.phone}</p>
+                <div className="btn3">
+                    <button>edit</button>
+                    <button>delete</button>
+                </div>
+            </div>
+        </div>
+    )
+}
